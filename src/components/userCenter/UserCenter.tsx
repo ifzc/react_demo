d5013817f16d4fb47bbc3a6a82d696c8d15387b8
xujuanjuan <xujuanjuan@duoyinsu.com>
2020-12-30 17:44:27 +0800

    个人中心 样式引入

--- src/components/userCenter/UserCenter.tsx
+++ src/components/userCenter/UserCenter.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { Layout, Menu} from 'antd';
+import '../../routes/userCenter/userCenter.scss'
 const { Sider } = Layout;
 
 class UserCenter extends React.Component {
@@ -10,13 +11,13 @@ class UserCenter extends React.Component {
         <Sider>
         <div className="personal-center">个人中心</div>
         <Menu mode="inline">
-              <Menu.Item key="1">
+              <Menu.Item key="/user/info">
                 <Link to="/user/info">基本资料</Link>
               </Menu.Item>
-              <Menu.Item key="2">
+              <Menu.Item key="/user/settings">
                 <Link to="/user/settings">安全设置</Link>
               </Menu.Item>
-              <Menu.Item key="3">
+              <Menu.Item key="/user/log">
                 <Link to="/user/log">操作日志</Link>
               </Menu.Item>
               <Menu.Item key="4">

7a6207ef496b0225b9d13c9db89a7e44f8186323
xujuanjuan <xujuanjuan@duoyinsu.com>
2020-12-30 15:20:52 +0800

    个人中心组件

---
+++ src/components/userCenter/UserCenter.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { Layout, Menu} from 'antd';
+const { Sider } = Layout;
+
+class UserCenter extends React.Component {
+
+  render() {
+    return (
+        <Sider>
+        <div className="personal-center">个人中心</div>
+        <Menu mode="inline">
+              <Menu.Item key="1">
+                <Link to="/Personal/BasicData">基本资料</Link>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Link to="/Personal/SetUp">安全设置</Link>
+              </Menu.Item>
+              <Menu.Item key="3">
+                <Link to="/Personal/log">操作日志</Link>
+              </Menu.Item>
+              <Menu.Item key="4">
+                <Link to="/Page2">共享账号</Link>
+              </Menu.Item>
+            </Menu>
+        </Sider>
+    );
+  }
+}
+export default UserCenter

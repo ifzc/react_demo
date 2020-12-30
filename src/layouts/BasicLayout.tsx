ac17b39ba9921077d46e2de79c2169dafae08f83
xujuanjuan <xujuanjuan@duoyinsu.com>
2020-12-30 17:38:47 +0800

    布局样式调整

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -70,10 +70,10 @@ class BasicLayout extends React.Component<any, any> {
                         </Header>
                         <Content style={{ margin: '0 16px' }}>
                             <Breadcrumb style={{ margin: '16px 0' }}>
-                                <Breadcrumb.Item>User</Breadcrumb.Item>
-                                <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                                {/* <Breadcrumb.Item>User</Breadcrumb.Item>
+                                <Breadcrumb.Item>Bill</Breadcrumb.Item> */}
                             </Breadcrumb>
-                            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                            <div className="site-layout-background" style={{ minHeight: 360 }}>
                                 <RouterSwitch />
                             </div>
                         </Content>

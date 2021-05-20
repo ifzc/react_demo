856533167720f30925fe050c99aa5cc66d504842
xujuanjuan <xujuanjuan@duoyinsu.com>
2021-05-20 14:29:25 +0800

    router修改

--- src/layouts/RouterSwitch.tsx
+++ src/layouts/RouterSwitch.tsx
@@ -12,17 +12,6 @@ function RouterSwitch() {
         return (
             <Suspense fallback={<div className="suspense-box"><Spin size="large" /></div>}>
                 <Switch>
-                    {/* <Route path='/dashboard' exact render={() => (
-                        <Redirect to='/dashboard' />
-                    )} />
-                    <Route path='/to' component={TodoList}/>
-                    <Route path='/assets' component={AssetsTable} />
-                    <Route path='/dashboard' component={Dashboard} />
-                    <Route path='/user/info' component={UserInfo} />
-                    <Route path='/user/settings' component={UserSettings} />
-                    <Route path='/user/log' component={UserLog} />
-                    <Route path='/user/share' component={ShareAccount} />
-                    <Route path='/agent' component={Open} /> */}
                     {listItems}
                 </Switch>
             </Suspense>

65fc1cb1b2d13a18aba2c142997679725215544b
xujuanjuan <xujuanjuan@duoyinsu.com>
2021-05-27 14:36:47 +0800

    router

--- src/layouts/RouterSwitch.tsx
+++ src/layouts/RouterSwitch.tsx
@@ -1,21 +1,40 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Spin } from 'antd'
-import { routes } from '../router';
 import { Route, Switch } from 'react-router-dom';
-
+const Open = lazy(() => import('../routes/open'));
+/* const LoginLayout = lazy(() => import('../layouts/LoginLayout')); */
+const Dashboard = lazy(() => import('../routes/dashboard'));
+const UserInfo = lazy(() => import('../routes/userCenter/userInfo'));
+const UserSettings = lazy(() => import('../routes/userCenter/settings'));
+const UserLog = lazy(() => import('../routes/userCenter/log'));
+const ShareAccount = lazy(() => import('../routes/userCenter/share'));
+const AssetsTable = lazy(() => import('../routes/assets'));
+const TodoList = lazy(() => import('../routes/assets/test'));
+//top
+const Help = lazy(() => import('../routes/top/help'));
+const Introduction = lazy(() => import('../routes/top/introduction'));
+const Guide = lazy(() => import('../routes/top/guide'));
 function RouterSwitch() {
-  const numbers = routes;
-  const listItems = numbers.map((item:any,index:number) =>
-    <Route path={item.path} component={item.name} key={index} />
-  );
-  console.log(listItems)
         return (
             <Suspense fallback={<div className="suspense-box"><Spin size="large" /></div>}>
                 <Switch>
-                    {listItems}
+                    {/* <Route path='/dashboard' exact render={() => (
+                        <Redirect to='/dashboard' />
+                    )} /> */}
+                    <Route path='/to' component={TodoList}/>
+                    <Route path='/assets' component={AssetsTable} />
+                    <Route path='/dashboard' component={Dashboard} />
+                    <Route path='/user/info' component={UserInfo} />
+                    <Route path='/user/settings' component={UserSettings} />
+                    <Route path='/user/log' component={UserLog} />
+                    <Route path='/user/share' component={ShareAccount} />
+                    <Route path='/agent' component={Open} />
+                    <Route path='/help' component={Help} />
+                    <Route path='/introduction' component={Introduction} />
+                    <Route path='/guide' component={Guide} />
                 </Switch>
             </Suspense>
         )
-}
+                  }
 
 export default RouterSwitch;
